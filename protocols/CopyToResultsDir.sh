0cb5e73f5092a3cd86ac7c0f3c40c7900a9fab95
BenjaminsM <m.benjamins@umcg.nl>
2018-04-20 14:00:53 +0200

    updated protocols for first version of GAP Pipeline

--- protocols/CopyToResultsDir.sh
+++ protocols/CopyToResultsDir.sh
@@ -2,12 +2,10 @@
 
 #MOLGENIS walltime=05:59:00 mem=10gb ppn=6
 
-
-
 #string intermediateDir
 #string resultDir
 #string Project
-
+#string diagnosticOutputFolder
 
 set -e
 set -u
@@ -18,3 +16,10 @@ set -u
 
 rsync -a "${intermediateDir}/${Project}_PennCNV.txt" "${resultDir}"
 rsync -a "${intermediateDir}/Callrates_${Project}.txt" "${resultDir}"
+
+#Copying files to Diagnostics output folder so DARWIN can further process the output files
+
+mkdir -p "${diagnosticOutputFolder}/${Project}"
+
+rsync -a "${intermediateDir}/${Project}_PennCNV.txt" "${diagnosticOutputFolder}/${Project}"
+rsync -a "${intermediateDir}/Callrates_${Project}.txt" "${diagnosticOutputFolder}/${Project}"
