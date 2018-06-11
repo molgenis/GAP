a0c99272c73d1b8b281797281a5092181cd1e304
gerbenvandervries <gerbenvandervries@gmail.com>
2018-06-11 12:51:18 +0200

    added more columns to finalReport

--- scripts/gtc_final_report.py
+++ scripts/gtc_final_report.py
@@ -39,7 +39,7 @@ with open(args.output_file, "w") as output_handle:
     output_handle.write(delim.join(["Total Samples", str(len(samples))]) + "\n")
 
     output_handle.write("[Data]\n")
-    output_handle.write(delim.join(["SNP Name", "Sample ID", "Chr", "MapInfo" ,"Snp" , "GType" ,"Allele1 - Top","Allele2 - Top","X_raw","Y_raw","X","Y"]) + "\n")
+    output_handle.write(delim.join(["SNP Name", "Sample ID", "Chr", "MapInfo" ,"SNP" , "GType" ,"Allele1 - Top","Allele2 - Top","X_raw","Y_raw","X","Y","B Allele Freq","Log R Ratio","GT Score","Alleles - Plus", "Alleles - Forward"]) + "\n")
     for gtc_file in samples:
         sys.stderr.write("Processing " + gtc_file + "\n")
         gtc_file = os.path.join(args.gtc_directory, gtc_file)
@@ -49,11 +49,17 @@ with open(args.output_file, "w") as output_handle:
         raw_Ys = gtc.get_raw_y_intensities()
 	normalized_intensities = GenotypeCalls( gtc_file ).get_normalized_intensities(manifest.normalization_lookups)
 	base_calls = GenotypeCalls( gtc_file ).get_base_calls()
+	logratios = GenotypeCalls( gtc_file ).get_logr_ratios()
+	BAFs = GenotypeCalls( gtc_file ).get_ballele_freqs()
+	genotype_scores = GenotypeCalls( gtc_file ).get_genotype_scores()
+        plus_strand_genotypes = gtc.get_base_calls_plus_strand(manifest.snps, manifest.ref_strands)
+        forward_strand_genotypes = gtc.get_base_calls_forward_strand(manifest.snps, manifest.source_strands)
+
         assert len(genotypes) == len(manifest.names)
-        for (name, chrom, map_info, snp, genotype, norm, Allele, raw_x, raw_y) in zip(manifest.names, manifest.chroms, manifest.map_infos, manifest.snps ,genotypes , normalized_intensities, base_calls, raw_Xs, raw_Ys):
+        for (name, chrom, map_info, snp, genotype, norm, Allele, raw_x, raw_y, BAF, logratio , genotype_score , ref_strand_genotype, source_strand_genotype) in zip(manifest.names, manifest.chroms, manifest.map_infos, manifest.snps ,genotypes , normalized_intensities, base_calls, raw_Xs, raw_Ys, BAFs , logratios , genotype_scores , plus_strand_genotypes, forward_strand_genotypes):
 
             x_norm = norm[0]
             y_norm = norm[1]
 
-            output_handle.write(delim.join([name, os.path.basename(gtc_file)[:-4], str(chrom) , str(map_info) , str(snp), code2genotype[genotype] , Allele[:-1] , Allele[1:] , str(raw_x), str(raw_y) ,str(x_norm), str(y_norm)]) + "\n")
+            output_handle.write(delim.join([name, os.path.basename(gtc_file)[:-4], str(chrom) , str(map_info) , str(snp), code2genotype[genotype] , Allele[:-1] , Allele[1:] , str(raw_x), str(raw_y) ,str(x_norm), str(y_norm),str(BAF),str(logratio),str(genotype_score),ref_strand_genotype, source_strand_genotype]) + "\n")
 
