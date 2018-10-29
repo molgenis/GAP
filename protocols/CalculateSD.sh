367a380934e7dd8bd502448a5f4e608fcc1fb9b3
kdelange <kimatwork01@gmail.com>
2018-10-29 16:01:33 +0100

    removed concordencchechB script and modified indentation CalculatedSD.sh

--- protocols/CalculateSD.sh
+++ protocols/CalculateSD.sh
@@ -53,31 +53,31 @@ done
 for input_file in $(find "${intermediateDir}"/[0-9]*_R*.gtc.txt)
 do
 
-        log_ratios=$(awk '{if ($3 != "X" && $3 != "Y" && $3 != "XY" ) print $6}' "${input_file}")
-        sd=$(echo "${log_ratios[@]}" | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}')
+    log_ratios=$(awk '{if ($3 != "X" && $3 != "Y" && $3 != "XY" ) print $6}' "${input_file}")
+    sd=$(echo "${log_ratios[@]}" | awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}')
 
-        echo "$(basename ${input_file}): ${sd}"
+    echo "$(basename ${input_file}): ${sd}"
 
-        for sample_barcode in ${barcodelist[@]}
-        do
-            filename=$(basename ${input_file})
-            barcode_position=${filename%%.*}
-            barcode=${barcode_position%_*}
-            position=${barcode_position##*_}
+    for sample_barcode in ${barcodelist[@]}
+    do
+        filename=$(basename ${input_file})
+        barcode_position=${filename%%.*}
+        barcode=${barcode_position%_*}
+        position=${barcode_position##*_}
 
-            sample_id=$(echo "${sample_barcode}" | awk 'BEGIN {FS=":"}{print $1}')
-            barcode_combined=$(echo "${sample_barcode}" | awk 'BEGIN {FS=":"} {print $2}')
-            sentrix_barcode=$(echo "${barcode_combined}" | awk 'BEGIN {FS="_"}{print $1}')
-            sentrix_position=$(echo "${barcode_combined}" | awk 'BEGIN {FS="_"}{print $2}')
+        sample_id=$(echo "${sample_barcode}" | awk 'BEGIN {FS=":"}{print $1}')
+        barcode_combined=$(echo "${sample_barcode}" | awk 'BEGIN {FS=":"} {print $2}')
+        sentrix_barcode=$(echo "${barcode_combined}" | awk 'BEGIN {FS="_"}{print $1}')
+        sentrix_position=$(echo "${barcode_combined}" | awk 'BEGIN {FS="_"}{print $2}')
 
 
-            if  [[ "${sd}" < 0.2 && "${barcode}" == "${sentrix_barcode}" && "${position}" == "${sentrix_position}" ]]
-            then
-                echo "${intermediateDir}/${input_file} ${concordanceInputDir}/${sample_id}.vcf"
-                mv "${intermediateDir}/${filename}.vcf" "${concordanceInputDir}/${sample_id}.vcf"
-                awk '{OFS="\t"}{if ($0 ~ "#CHROM" ){ print $1,$2,$3,$4,$5,$6,$7,$8,$9,"'$sample_id'"} else {print $0}}' "${concordanceInputDir}/${sample_id}.vcf" > "${concordanceInputDir}/${sample_id}.FINAL.vcf"
-                bgzip -c "${concordanceInputDir}/${sample_id}.FINAL.vcf" > "${concordanceInputDir}/${sample_id}.FINAL.vcf.gz"
-                tabix -p vcf "${concordanceInputDir}/${sample_id}.FINAL.vcf.gz"
-            fi
-        done
+        if  [[ "${sd}" < 0.2 && "${barcode}" == "${sentrix_barcode}" && "${position}" == "${sentrix_position}" ]]
+        then
+            echo "${intermediateDir}/${input_file} ${concordanceInputDir}/${sample_id}.vcf"
+            mv "${intermediateDir}/${filename}.vcf" "${concordanceInputDir}/${sample_id}.vcf"
+            awk '{OFS="\t"}{if ($0 ~ "#CHROM" ){ print $1,$2,$3,$4,$5,$6,$7,$8,$9,"'$sample_id'"} else {print $0}}' "${concordanceInputDir}/${sample_id}.vcf" > "${concordanceInputDir}/${sample_id}.FINAL.vcf"
+            bgzip -c "${concordanceInputDir}/${sample_id}.FINAL.vcf" > "${concordanceInputDir}/${sample_id}.FINAL.vcf.gz"
+            tabix -p vcf "${concordanceInputDir}/${sample_id}.FINAL.vcf.gz"
+        fi
+    done
 done
