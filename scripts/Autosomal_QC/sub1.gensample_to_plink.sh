d05aa58535023b86b2945d696ae1d5f9c3fe7e05
ealopera <44577866+ealopera@users.noreply.github.com>
2022-04-01 13:36:17 +0200

    Update sub1.gensample_to_plink.sh

--- scripts/Autosomal_QC/sub1.gensample_to_plink.sh
+++ scripts/Autosomal_QC/sub1.gensample_to_plink.sh
@@ -21,21 +21,17 @@ module load PLINK/1.9-beta6-20190617
 ### inform deifnition of variables 
 echo "wkdir =" ${1}  ## the place used to process the data and make output
 echo "inputdir=" ${2}  ## directory with the folders fo each batch
-echo "out=" ${3} ### name of the output folder
-echo "logs=" ${1}/${3}/logs ## a logfiles directory
-echo "chr=" ${4} ### cromosome processed
+echo "chr=" ${3} ### cromosome processed
 
 ################################################################################
 ### Main
 ################################################################################
-mkdir -p ${1}/${3}/ #make results directory
-mkdir -p ${1}/${3}/logs/ #make logs directory
-mkdir -p ${1}/${3}/meta/ # make directory for metafiles (used independently of chromosomes)
+mkdir -p ${1}/${2}/meta/ # make directory for metafiles (used independently of chromosomes)
 
-      plink --data ${2}/chr_${4} \
+      plink --data ${2}/chr_${3} \
             --make-bed  \
-            --out ${1}/${3}/chr_${4}
-      echo "${1}/${3}/chr_${4}" >> ${1}/${3}/meta/allb_${4}.list
+            --out ${1}/chr_${3}
+      echo "${1}/chr_${3}" >> ${1}/meta/allb_${3}.list
 
 
 
